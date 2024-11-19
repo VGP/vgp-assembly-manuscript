a2aa88703b794cfade0402a316bc7562d6ad32b2
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-19 11:14:54 -0500

    distance matrix

---
+++ mash/chromosomes/distance_matrix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+readarray -t in < $1 # input list
+
+for (( i=0; i<${#in[@]}; i++ )); do # triangular matrix
+    for (( j=0; j<${#in[@]}; j++ )); do
+        if [ $j -gt $i ]; then # sketch and get the dist
+        	accession1=${in[$i]%%$'\t'*}
+        	accession2=${in[$j]%%$'\t'*}
+          awk -v acc1="$accession1" -v acc2="$accession2" '{print $acc1 $acc2 $0}' test.ls <(mash dist sketches/$accession1* sketches/$accession2*)
+        fi
+    done
+done
