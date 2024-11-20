aa6066448de37047c08f2095659cb9265cc3af67
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-20 09:37:58 -0500

    Update distance_hist.py

--- mash/chromosomes/distance_hist.py
+++ mash/chromosomes/distance_hist.py
@@ -6,5 +6,5 @@ import sys
 df = pd.read_csv(sys.argv[1], header=None, sep="\t", names=['Accession 1', 'Accession 2', 'Chr2', 'Chr1', 'D', 'P', 'Hashes'])
 
 # creating a histogram
-plt.hist(df['D'], bins='sturges')
+plt.hist(df['D'], bins=100)
 plt.savefig('mash_histogram_distances.png')
