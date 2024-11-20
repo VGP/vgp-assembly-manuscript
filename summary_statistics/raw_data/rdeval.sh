2197787d71d4a7935bfdaad9bf25f3330a97bdb9
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-20 11:40:58 -0500

    prettify

--- summary_statistics/raw_data/rdeval.sh
+++ summary_statistics/raw_data/rdeval.sh
@@ -20,18 +20,20 @@ do
   printf "Found records:\n"
   cat accessions.ls
 
-  while read -r -u 4 SAMPLE SRR SRX INSTR LIB_STR
+  while read -r -u 4 EXPERIMENT SRR SRX INSTR LIB_STR
     do
         if [ ! -f $SRR.fastq ]; then
-          printf "downloading: $SAMPLE\t$SRR\t$SRX\n" "$SAMPLE" "$SRR" "$SRX"
+          printf "downloading: %s\t%s\t%s\n" "$EXPERIMENT" "$SRR" "$SRX"
           fasterq-dump $SRR
         fi
     done 4< <(grep SMRT accessions.ls | awk '{if ($6!=0) print}')
 
-  printf "$SAMPLE\t" >> rdeval.tsv
+  printf "Computing summary statistics:\n"
+  printf "%s\t" "$SRA" >> rdeval.tsv
   rdeval -r *.fastq | awk -F': ' '{print $2}' | sed 1d | sed -z 's/\n/\t/g; s/.$//' >> rdeval.tsv
 
-  printf "$SAMPLE\t" >> rdevalCumInv.tsv
+  printf "Computing Cumulative inverse distribution:\n"
+  printf "%s\t" "$SRA" >> rdevalCumInv.tsv
   rdeval *.fastq -s c | sed -z 's/\n/;/g' >> rdevalCumInv.tsv
 
 	rm -f *.fastq
