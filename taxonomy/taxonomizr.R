7b13596414f757ce63cab36d36938537d0ef6f0e
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-21 17:07:49 -0500

    Update taxonomizr.R

--- taxonomy/taxonomizr.R
+++ taxonomy/taxonomizr.R
@@ -35,7 +35,7 @@ taxa_VGP<-getTaxonomy(table2[,1],'accessionTaxa.sql')
 VGP_tree <- makeNewick(taxa_VGP)
 write(VGP_tree, file = "VGP_tree.nwk")
 
-descendants_all <- getId(getDescendants(7742,'accessionTaxa.sql'), 'accessionTaxa.sql')
+descendants_all <- getId(getDescendants(7742,'accessionTaxa.sql') %>% str_subset(pattern = " sp. "), 'accessionTaxa.sql')
 taxa_all<-getTaxonomy(descendants_all,'accessionTaxa.sql')
 
 full_tree <- makeNewick(taxa_all)
