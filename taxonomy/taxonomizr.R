62bd0d7cb08a4865b00076ed782664b043b8dc15
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-21 16:54:52 -0500

    Update taxonomizr.R

--- taxonomy/taxonomizr.R
+++ taxonomy/taxonomizr.R
@@ -23,23 +23,20 @@ table1 <- read.csv("accession_metadata.ls", header=FALSE, sep=",")
 names(table1) <- c("Accession", "Tolid", "Species")
 
 taxaId_Genbank<-accessionToTaxa(as.character(table1$Accession),"accessionTaxa.sql")
-taxons_Genbank<-getTaxonomy(taxaId_Genbank,'accessionTaxa.sql')
-table1 <- cbind(table1,taxaId_Genbank, taxons_Genbank)
+taxa_Genbank<-getTaxonomy(taxaId_Genbank,'accessionTaxa.sql')
+table1 <- cbind(table1,taxaId_Genbank, taxa_Genbank)
 
 table1 %>% as_tibble() %>% print(n=Inf, width=Inf)
 
-getDescendants(7742,'accessionTaxa.sql')
-
 table2 <- read.csv("data_freeze_ids.ls", header=FALSE)
-taxons_VGP<-getTaxonomy(table2[,1],'accessionTaxa.sql')
-print(taxons_VGP)
+taxa_VGP<-getTaxonomy(table2[,1],'accessionTaxa.sql')
+print(taxa_VGP)
 
-VGP_tree <- makeNewick(taxons_VGP)
+VGP_tree <- makeNewick(taxa_VGP)
 write(VGP_tree, file = "VGP_tree.nwk")
 
-raw <- getRawTaxonomy(7742,'accessionTaxa.sql')
-write.csv(raw,"raw.csv")
-normalized <- normalizeTaxa(raw, lineageOrder=c('infraorder','suborder','superorder','infraclass','subclass','class','subphylum'))
+descendants_all <- getDescendants(7742,'accessionTaxa.sql')
+taxa_all<-getTaxonomy(descendants_all,'accessionTaxa.sql')
 
-full_tree <- makeNewick(normalized)
+full_tree <- makeNewick(taxa_all)
 write(full_tree, file = "full_tree.nwk")
