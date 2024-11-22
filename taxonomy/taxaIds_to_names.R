b152c9fd8f1e6bf13006e494b3aa692643f81535
Giulio Formenti <giulio.formenti@gmail.com>
2024-11-21 20:51:28 -0500

    Create taxaIds_to_names.R

---
+++ taxonomy/taxaIds_to_names.R
@@ -0,0 +1,24 @@
+requiredpackages <- c("this.path", "data.table", "taxonomizr", "tidyverse")
+
+install_load <- function(packages){
+     for (p in packages) {
+          if (p %in% rownames(installed.packages())) {
+               library(p, character.only=TRUE)
+          } else {
+               install.packages(p, repos = "http://cran.us.r-project.org")
+               library(p,character.only = TRUE)
+          }
+     }
+}
+
+install_load(requiredpackages)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+#fetch NCBI db
+prepareDatabase('accessionTaxa.sql')
+
+#load data
+taxaIds <- read.csv(args[1], header=FALSE)
+taxa <- getTaxonomy(taxaIds,'accessionTaxa.sql')
+write(taxa, file = args[2])
