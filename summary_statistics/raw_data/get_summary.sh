4101f2b70eb911856c3d83514fdd4db7f0dd6a21
Giulio Formenti <giulio.formenti@gmail.com>
2025-01-09 16:37:22 -0500

    Create get_summary.sh

---
+++ summary_statistics/raw_data/get_summary.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+while IFS="," read -r -u 3 accession tolid SRA
+do
+  esearch -db sra -query $SRA | esummary | xtract -pattern DocumentSummary -element Sample@acc Run@acc Experiment@acc Platform instrument_model LIBRARY_STRATEGY Summary -element Statistics@total_bases | grep SMRT | grep 'WGS\|WGA' | awk '{if ($6!=0) print}'
+done 3< <(grep 'ERS\|SRS' raw_data_metadata.ls | grep -v alt)
