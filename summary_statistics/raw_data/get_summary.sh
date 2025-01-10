2a31e83419a54a42dbba3435d95a4a4e9f768020
Giulio Formenti <giulio.formenti@gmail.com>
2025-01-10 10:35:19 -0500

    Update get_summary.sh

--- summary_statistics/raw_data/get_summary.sh
+++ summary_statistics/raw_data/get_summary.sh
@@ -3,5 +3,5 @@ set -e
 
 while IFS="," read -r -u 3 accession tolid SRA
 do
-  esearch -db sra -query $SRA | esummary | xtract -pattern DocumentSummary -element Sample@acc Run@acc Experiment@acc Platform instrument_model LIBRARY_STRATEGY Summary -element Statistics@total_bases'
+  esearch -db sra -query $SRA | esummary | xtract -pattern DocumentSummary -element Sample@acc Run@acc Experiment@acc Platform instrument_model LIBRARY_STRATEGY Summary -element Statistics@total_bases
 done 3< <(grep 'ERS\|SRS' raw_data_metadata.ls | grep -v alt)
