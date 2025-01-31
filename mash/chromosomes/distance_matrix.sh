cb2e2cb1d9c91bb62560de75a22507c2244dc21c
Giulio Formenti <giulio.formenti@gmail.com>
2025-01-30 21:21:30 -0500

    avoid recomputing

--- mash/chromosomes/distance_matrix.sh
+++ mash/chromosomes/distance_matrix.sh
@@ -2,8 +2,6 @@
 
 THREAD="${2:-32}"
 
-rm -f distance_matrix.tsv
-
 readarray -t in < $1 # input list
 
 for (( i=0; i<${#in[@]}; i++ )); do # triangular matrix
@@ -11,6 +9,12 @@ for (( i=0; i<${#in[@]}; i++ )); do # triangular matrix
         if [ $j -gt $i ]; then # sketch and get the dist
         	accession1=${in[$i]%%$'\t'*}
         	accession2=${in[$j]%%$'\t'*}
+
+        	printf "Processing: ${accession1}\t${accession2}\n"
+          if grep -qwE "${accession1}.*${accession2}|${accession2}.*${accession1}" distance_matrix.tsv; then
+            printf "Already done. Skipping.\n"
+            continue
+          fi
           awk -v acc1="$accession1" -v acc2="$accession2" '{printf acc1"\t"acc2"\t"$0"\n"}' <(mash dist -p "$THREAD" sketches/$accession1* sketches/$accession2*) >> distance_matrix.tsv
         fi
     done
